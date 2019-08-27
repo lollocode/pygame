9fccbb07c73a770ab05eea80065de0ce37971139
lollocode <lollo.codeluppi@gmail.com>
2019-08-27 16:02:15 +0200

    add movemets

--- prova.py
+++ prova.py
@@ -1,21 +1,52 @@
 import pygame, sys
 
 pygame.init()
-
-screen = pygame.display.set_mode((640, 480))
-
+screen_x = 680
+screen_y = 480
+screen = pygame.display.set_mode((screen_x, screen_y))
 screen.fill([0,0,0])
-pygame.draw.rect(screen,[255,255,255],(200,150,100,50))
+
 pygame.display.set_caption("First Pygame Application")
 
 
+class Player:
+    def __init__ (self,x,y,color,lato):
+        self.x = x
+        self.y = y
+        self.color = color
+        self.lato = lato
+
+    def draw(self):
+        screen.fill([0,0,0])
+        pygame.draw.rect(screen,self.color,(self.x,self.y,self.lato,self.lato))
+
+    def move (self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    self.y -= self.lato
+                    self.draw()
+                if event.key == pygame.K_s and self.y < 480-self.lato:
+                    self.y += self.lato
+                    self.draw()
+                if event.key == pygame.K_a and self.x > 0:
+                    self.x -= self.lato
+                    self.draw()
+                if event.key == pygame.K_d and self.x+self.lato < screen_x:
+                    self.x += self.lato
+                    self.draw()
+
+		
+
+
+
+p = Player((screen_x/2)-20,400,[255,255,255],40)
+p.draw()
+		
 while True:
-
-	clock = pygame.time.Clock()
-
-	clock.tick(50)
-
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-	  		sys.exit()
-	pygame.display.flip()
+    clock = pygame.time.Clock()
+    clock.tick(30)
+    p.move()
+    pygame.display.update()
