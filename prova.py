c22bca73c7a4ef140bcbf86397169b2db378a7c5
lollocode <lollo.codeluppi@gmail.com>
2019-08-29 20:06:11 +0200

    il coso spara un po buggato ma spara

--- prova.py
+++ prova.py
@@ -4,49 +4,77 @@ pygame.init()
 screen_x = 680
 screen_y = 480
 screen = pygame.display.set_mode((screen_x, screen_y))
-screen.fill([0,0,0])
 
 pygame.display.set_caption("First Pygame Application")
 
 
-class Player:
+char = []
+
+class Entity:
     def __init__ (self,x,y,color,lato):
         self.x = x
         self.y = y
         self.color = color
         self.lato = lato
+ 
 
     def draw(self):
-        screen.fill([0,0,0])
-        pygame.draw.rect(screen,self.color,(self.x,self.y,self.lato,self.lato))
+        for i in char:
+            pygame.draw.rect(screen,i.color,(i.x,i.y,i.lato,i.lato))
 
+
+
+class Player(Entity):
+    def __init__(self,x,y,color,lato):
+        super().__init__(x,y,color,lato)
+ 
+
+class Bullet(Entity):
+    def __init__ (self,x,y,color,lato):
+    	super().__init__(x,y,color,lato)
+    
     def move (self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w:
-                    self.y -= self.lato
-                    self.draw()
-                if event.key == pygame.K_s and self.y < 480-self.lato:
-                    self.y += self.lato
-                    self.draw()
-                if event.key == pygame.K_a and self.x > 0:
-                    self.x -= self.lato
-                    self.draw()
-                if event.key == pygame.K_d and self.x+self.lato < screen_x:
-                    self.x += self.lato
-                    self.draw()
+        pygame.draw.rect(screen,[0,0,0],(self.x,self.y,self.lato,self.lato))
+        self.y -= self.lato
+        self.draw()
+        if(self.y<0):
+        	self.y = p.y-10
 
-		
 
 
 
 p = Player((screen_x/2)-20,400,[255,255,255],40)
-p.draw()
+b = Bullet(p.x+15,p.y-10,[0,255,0],10)
+char.append(p)
+char.append(b)
+
 		
 while True:
+    screen.fill([0,0,0])
+    p.draw()
+    b.move()
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    b.y -= p.lato
+                    p.y -= p.lato
+                    p.draw()
+                if event.key == pygame.K_s and p.y < 480-p.lato:
+                    b.y += p.lato
+                    p.y += p.lato
+                    p.draw()
+                   
+                if event.key == pygame.K_a and p.x > 0:
+                    b.x -= p.lato
+                    p.x -= p.lato
+                    p.draw()
+
+                if event.key == pygame.K_d and p.x+p.lato < screen_x:
+                    b.x += p.lato
+                    p.x += p.lato
+                    p.draw()
     clock = pygame.time.Clock()
-    clock.tick(30)
-    p.move()
+    clock.tick(60)
     pygame.display.update()
