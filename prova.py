c8942e0cedc21a59f345d3da72467173ee4f425f
lollocode <lollo.codeluppi@gmail.com>
2019-08-30 19:58:33 +0200

    mods

--- prova.py
+++ prova.py
@@ -9,21 +9,27 @@ py = 400
 bx = px+3
 by = py-20
 blato = 10
-ex = 100
+ex = 0
 ey = 40
 elato = 60
 plato = 40
 screen = pygame.display.set_mode((screen_x, screen_y))
 
 pygame.display.set_caption("First Pygame Application")
-back = pygame.image.load("./space.png")
+back1 = pygame.image.load("./space1.png")
+
 play = pygame.image.load("./starship.png")
 bull = pygame.image.load("./bullet.png")
 enem = pygame.image.load("./enemy.png")
 points = 0
 level = 1
 speed = 1
-char = []
+
+
+opt = ["e0"]
+enemy = []
+
+
 pygame.font.init()
 myfont = pygame.font.SysFont('arial', 20)
 
@@ -35,80 +41,73 @@ def scritte():
     textsurface = myfont.render("Tempo trascorso  "+str(elapsed_time), True, (255, 0, 0))
     punti = myfont.render("Il tuo punteggio "+ str(points), True,(255,0,0))
     livello = myfont.render("Livello "+ str(level), True,(255,0,0))
-    screen.blit(back,(0,0))
+    screen.blit(back1,(0,0))
     screen.blit(textsurface,(0,0))
     screen.blit(punti,(300,0))
     screen.blit(livello,(500,0))
 
+class Enemy:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+def generator(n):
+    opt.clear()
+    for i in range(0,n):
+        opt.append("e"+str(i))
+
+def seeder(n):
+    generator(n)
+    enemy.clear()
+    for i in opt:
+        i = Enemy(int(i[1])*100,ey)
+        enemy.append(i)
+def draw():
+    for i in enemy:
+        screen.blit(enem,(i.x,i.y))
+
 def move():
     global by
     global ey,ex,points,speed
-    by -= 10
+    by -= 10*level
     if by < 0:
         by = py-20
-    ey += speed
-    if points >= speed*5:
-        speed += 1
-    if ey > screen_y:
-        print("hai perso")
-        sys.exit()
+    for i in enemy:
+        i.y += speed/2
+        if points >= speed*5:
+            speed += 1
+        if i.y > screen_y:
+            print("hai perso")
+            sys.exit()
 
-class Entity:
-    def __init__ (self,x,y,color,lato):
-        self.x = x
-        self.y = y
-        self.color = color
-        self.lato = lato
- 
-
-class Player(Entity):
-    def __init__(self,x,y,color,lato):
-        super().__init__(x,y,color,lato)
- 
-
-class Bullet(Entity):
-    def __init__ (self,x,y,color,lato):
-    	super().__init__(x,y,color,lato)
+def change(n):
+    global back1,play
+    play = pygame.image.load("./starship"+str(n)+".png")
+    back1 = pygame.image.load("./space"+str(n)+".png")
     
-    def move (self):
-        self.y -= self.lato/3
-        self.draw()
-        if(self.y<0):
-        	self.y = p.y-self.lato
-
-
-class Enemy(Entity):
-    def __init__(self,x,y,color,lato):
-        super().__init__(x,y,color,lato)
-
-    def move(self):
-        self.y += 1
-        
 
 
 
-p = Player((screen_x/2)-20,400,[255,255,255],40)
-b = Bullet(p.x+15,p.y-10,[0,255,0],10)
-e = Enemy(randint(0,screen_x-p.lato),20,[255,0,0],40)
-char.append(p)
-char.append(b)
-char.append(e)
+e = Enemy(0,40)
+enemy.append(e)
 
-		
 while True:
+    if points == 15:
+        level += 1
+        points = 0
+        seeder(level)
+        change(level)
     scritte()
+    
     screen.blit(play,(px,py))
     screen.blit(bull,(bx,by))
-    screen.blit(enem,(ex,ey))
+    draw()
     move()
-    if (bx +blato >= ex and bx <= ex+elato) and (by >= ey and by  <= ey+elato):
-        ey = 20
-        ex = randint(0,9)*elato
-        points += 1
-    if(e.x + e.lato >= p.x and e.x <= p.x+p.lato) and (e.y >= p.y and e.y < p.y+p.lato) or (e.y >= screen_y):
-        print("hai perso")
-        print("il tuo tempo:",elapsed_time)
-        sys.exit()
+    for i in enemy:
+        if (bx +blato >= i.x and bx <= i.x+elato) and (by >= i.y and by  <= i.y+elato):
+            i.y = 20
+            i.x = randint(0,9)*elato
+            points += 1
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
